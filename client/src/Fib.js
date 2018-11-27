1d006b3ed2e335d7eb7fede39723d46701de34b8
Fernando Perez <fernando.perez@opskins.com>
2018-11-26 21:30:59 -0300

    initial commit

---
+++ client/src/Fib.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Fib extends Component {
+  state = {
+    seenIndexes: [],
+    values: {},
+    index: ""
+  };
+  componentDidMount = () => {
+    this.fetchValue();
+    this.fetchIndexes();
+  };
+
+  fetchValue = async () => {
+    const values = await axios.get("/api/values/current");
+    this.setState({ values: values.data });
+  };
+
+  fetchIndexes = async () => {
+    const values = await axios.get("/api/values/all");
+    this.setState({ seenIndexes: values.data });
+  };
+
+  renderSeenIndexes = () => {
+    return this.state.seenIndexes.map(({ number }) => number).join(", ");
+  };
+
+  renderValues = () => {
+    const entries = [];
+    for (let key in this.state.values) {
+      entries.push(<div key={key}>I calculated {this.state.values[key]}</div>);
+    }
+    return entries;
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    await axios.post("/api/values", {
+      index: this.state.index
+    });
+    this.setState({ index: "" });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Enter your index</label>
+          <input
+            value={this.state.index}
+            onChange={event => this.setState({ index: event.target.value })}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <h3>Indexes i have seen</h3>
+        {this.renderSeenIndexes()}
+        <h3>Calculated values:</h3>
+        {this.renderValues()}
+      </div>
+    );
+  }
+}
+
+export default Fib;
---
+++ client/src/OtherPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { route } from 'react-router-dom';
+
+const OtherPage = (props) => {
+    return (
+        <div>
+            Other page
+        </div>
+    )
+};
+
+export default OtherPage;
